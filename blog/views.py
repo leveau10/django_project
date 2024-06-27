dc5a4ea26785d33e661c6108a0461a2be9431533
leveau10 <leovir957@gmail.com>
2024-06-27 15:56:34 -0300

    feat: add db, migrations and real data

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2024'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2024'
-    }
-]
-
-
+from .models import Post
+from django.contrib.auth.models import User
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
